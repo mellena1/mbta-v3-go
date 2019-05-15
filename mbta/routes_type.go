857c4912882afd82af5e672ec840122c731bc81d
charles <charlesarnaudo@gmail.com>
2019-05-15 09:42:13 -0400

    json attr for route

--- mbta/routes_type.go
+++ mbta/routes_type.go
@@ -1,6 +1,6 @@
 package mbta
 
-// RouteType enum for possible Route types
+// RouteType enum for possible Route types (see https://github.com/google/transit/blob/master/gtfs/spec/en/reference.md#routestxt)
 type RouteType int
 
 const (
@@ -24,13 +24,15 @@ const (
 
 // Route holds all the info about a given MBTA Route
 type Route struct {
-	ID          string
-	AgencyID    string
-	ShortName   string
-	LongName    string
-	Description string
-	Type        RouteType
-	Color       string
-	TextColor   string
-	SortOrder   int
+	ID                    string    `jsonapi:"primary,route"`
+	Color                 string    `jsonapi:"attr,color"`
+	Description           string    `jsonapi:"attr,description"`
+	DirectionDestinations []string  `jsonapi:"attr,direction_destinations"`
+	DirectionNames        []string  `jsonapi:"attr,direction_names"`
+	LongName              string    `jsonapi:"attr,long_name"`
+	SortOrder             int       `jsonapi:"attr,sort_order"`
+	TextColor             string    `jsonapi:"attr,text_color"`
+	Type                  RouteType `jsonapi:"attr,type"`
+	ShortName             string    `jsonapi:"attr,short_name"`
+	// Line				  Line	    `jsonapi:"relation,line"`
 }
