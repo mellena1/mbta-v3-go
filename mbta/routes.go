671c53b6850454560b0404e987cabe9b6cc4385c
charles <charlesarnaudo@gmail.com>
2019-05-19 16:25:24 -0400

    wiring up

--- mbta/routes.go
+++ mbta/routes.go
@@ -1,19 +1,54 @@
 package mbta
 
+import (
+	"context"
+	"fmt"
+)
+
 const routesAPIPath = "/routes"
 
 // RouteService handling all of the route related API calls
 type RouteService service
 
-func (s *RouteService) GetAllRoutes() {
-
+// GetAllRoutes returns all routes from the mbta API
+func (s *RouteService) GetAllRoutes(config GetAllRoutesRequestConfig) ([]*Route, error) {
+	return s.GetAllRoutesWithContext(context.Background(), config)
 }
 
-func (s *RouteService) GetAllRoutesWithContext() {
+// GetAllRoutesWithContext returns all routes from the mbta API given a context
+func (s *RouteService) GetAllRoutesWithContext(ctx context.Context, config GetAllRoutesRequestConfig) ([]*Route, error) {
+	req, err := s.client.newGETRequest(routesAPIPath)
+	config.addHTTPParamsToRequest(req)
+	req = req.WithContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	untypedRoutes, _, err := s.client.doManyPayload(req, Route{})
+	routes := make([]*Route, len(untypedRoutes))
+	for i := 0; i < len(untypedRoutes); i++ {
+		routes[i] = untypedRoutes[i].(*Route)
+	}
+	return routes, err
 }
 
-func (s *RouteService) GetRoute() {
+// GetRoute return a stop from the mbta API
+func (s *RouteService) GetRoute(id string, config GetRouteRequestConfig) (*Route, error) {
+	return s.GetRouteWithContext(context.Background(), id, config)
 }
 
-func (s *RouteService) GetRouteWithContext() {
+// GetRouteWithContext return a stop from the mbta API given a context
+func (s *RouteService) GetRouteWithContext(ctx context.Context, id string, config GetRouteRequestConfig) (*Route, error) {
+	path := fmt.Sprintf("%s/%s", stopsAPIPath, id)
+	req, err := s.client.newGETRequest(path)
+	config.addHTTPParamsToRequest(req)
+	req = req.WithContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var route Route
+	_, err = s.client.doSinglePayload(req, &route)
+	return &route, err
+
 }
