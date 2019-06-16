3185321f407313991539acfc82544ba2f7adac46
mellena1 <mellena1@wit.edu>
2019-06-16 18:43:09 -0400

    Add getallservices test

--- mbta/services_test.go
+++ mbta/services_test.go
@@ -36,6 +36,52 @@ func Test_GetService(t *testing.T) {
 	equals(t, expected, actual)
 }
 
-func TestGetAllServices(t *testing.T) {
+func Test_GetAllServices(t *testing.T) {
+	addedDates1, _ := parseISO8601TimeDateOnlySlice([]string{"2019-05-28", "2019-05-29", "2019-05-30", "2019-05-31"})
+	endDate1, _ := parseISO8601TimeDateOnly("2019-06-21")
+	startDate1, _ := parseISO8601TimeDateOnly("2019-05-27")
+	removedDates1, _ := parseISO8601TimeDateOnlySlice([]string{"2019-05-27"})
 
+	addedDates2, _ := parseISO8601TimeDateOnlySlice([]string{"2019-06-01"})
+	endDate2, _ := parseISO8601TimeDateOnly("2019-06-22")
+	startDate2, _ := parseISO8601TimeDateOnly("2019-05-27")
+	expected := []*Service{
+		&Service{
+			ID:                 "BUS22019-hbb29011-Weekday-02",
+			AddedDates:         timeSliceToTimeISO8601Slice(addedDates1),
+			AddedDatesNotes:    []*string{nil, nil, nil, nil},
+			Description:        "Weekday schedule",
+			EndDate:            timeToTimeISO8601(endDate1),
+			RemovedDates:       timeSliceToTimeISO8601Slice(removedDates1),
+			RemovedDatesNotes:  []string{"Memorial Day"},
+			ScheduleName:       "Weekday",
+			ScheduleType:       "Weekday",
+			ScheduleTypicality: 1,
+			StartDate:          timeToTimeISO8601(startDate1),
+			ValidDays:          []Weekday{Monday, Tuesday, Wednesday, Thursday, Friday},
+		},
+		&Service{
+			ID:                 "BUS22019-hbb29016-Saturday-02",
+			AddedDates:         timeSliceToTimeISO8601Slice(addedDates2),
+			AddedDatesNotes:    []*string{nil},
+			Description:        "Saturday schedule",
+			EndDate:            timeToTimeISO8601(endDate2),
+			RemovedDates:       nil,
+			RemovedDatesNotes:  []string{},
+			ScheduleName:       "Saturday",
+			ScheduleType:       "Saturday",
+			ScheduleTypicality: 1,
+			StartDate:          timeToTimeISO8601(startDate2),
+			ValidDays:          []Weekday{Saturday},
+		},
+	}
+	server := httptest.NewServer(handlerForServer(t, servicesAPIPath))
+	defer server.Close()
+
+	mbtaClient := NewClient(ClientConfig{BaseURL: server.URL})
+	mbtaClient.client = server.Client()
+
+	actual, _, err := mbtaClient.Services.GetAllServices(&GetAllServicesRequestConfig{})
+	ok(t, err)
+	equals(t, expected, actual)
 }
